439a7b1cc735f3513f079c6541ed331c1bc29bdc
CrazyUrusai <skorpion8909@gmail.com>
2018-11-19 23:25:35 +0100

    Added new manager class, small improvments,35% done.

---
+++ GeneticsAlgorithms/EvolutionManager.py
@@ -0,0 +1,24 @@
+"""
+    
+    This class governs process of looking for better
+    combination making a path for a traveler
+    
+
+"""
+
+
+#-------------------------------------------------------------------------------------------
+class EvolutionManager:
+    def __init__(self, numOfGenerations,population):
+        """Takes number of generations and Population() object"""
+        self.numOfGenerations = numOfGenerations
+        self.population = population
+#-------------------------------------------------------------------------------------------
+    def startTraining(self):
+        """Start training sequence"""
+        for x in range(0,self.numOfGenerations):
+            self.population.nextGeneration()
+            print("Iteration num ",x,population)
+#-------------------------------------------------------------------------------------------
+#     def 
+#-------------------------------------------------------------------------------------------
